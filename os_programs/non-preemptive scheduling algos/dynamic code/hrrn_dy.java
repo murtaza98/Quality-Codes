83f29699abe682311493cbf78fc05e3a3105f7b6
Murtaza Patrawala <murtazaforuall@yahoo.in>
2018-09-28 08:30:09 +0530

    hrrn non-preemptive version

---
+++ os_programs/non-preemptive scheduling algos/dynamic code/hrrn_dy.java
@@ -0,0 +1,306 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class hrrn_dy
+{
+	public static void main(String[] args) throws Exception{
+		Time time = new Time();
+		Input ip = new Input(time);
+
+		InputThread ipThread = new InputThread(ip,time);
+		ExecutionThread execThread = new ExecutionThread(ip,time);
+
+		ipThread.start();
+		ipThread.join();
+		execThread.start();
+	}
+}
+
+class ExecutionThread extends Thread
+{
+	Input ip;
+	Time time;
+
+	int current_running_pid = -1;
+	int current_running_process_time_left = -1;
+	Process currentRunningProcess = null;
+
+	public ExecutionThread(Input ip,Time time){
+		this.ip = ip;
+		this.time = time;
+	}
+
+	public void run(){
+		while(time.isOver()){
+			int current_time = time.getTime();
+			checkAllProcessForArrival(current_time);
+			if(current_running_pid == -1){
+				//no process are currently scheduled
+
+				Process processToSchedule = ip.getProcessForScheduling();			 //get the first element from queue
+				if(processToSchedule!=null){
+					currentRunningProcess = processToSchedule;
+					current_running_pid = processToSchedule.getPid();
+					current_running_process_time_left = processToSchedule.getBurstTime();
+				}
+			}else{
+				//a process is scheduled so do nothing							
+			}
+
+			updateDisplay(current_time);
+
+			current_running_process_time_left--;    //decreament current running process burst time left
+			checkForProcessCompletion(current_time);
+
+			// ip.printAllProcess();
+
+			time.increamentTime();
+		}
+		ip.printAllProcessSummary();
+	}
+
+	public void updateDisplay(int currentTime){
+		System.out.println("Time: " + currentTime + " Process Scheduled " + current_running_pid);
+	}
+
+	public void checkAllProcessForArrival(int currentTime){
+		Iterator i = ip.getNewQueue().iterator();
+		while(i.hasNext()){
+			Process p =(Process)i.next();
+			if(p.getArrivalTime()==currentTime){
+				ip.insertIntoArrivedQueue(p);
+			}
+		}
+	}
+
+	public void checkForProcessCompletion(int currentTime){
+		if(current_running_process_time_left == 0){
+			current_running_pid = -1;
+			currentRunningProcess.setCompletionTime(currentTime);
+			ip.insertIntoCompletedQueue(currentRunningProcess);
+			currentRunningProcess = null;
+		}
+	}
+}
+
+class Input
+{
+	SortedList newQueue;
+	SortedList arrivedProcess;
+	SortedList completedProcess;
+
+	public Input(Time time){
+		this.newQueue = new SortedList(new SortByArrivalTime());
+		this.arrivedProcess = new SortedList(new SortByResponseRatio(time));
+		this.completedProcess = new SortedList(new SortByResponseRatio(time));
+	}
+
+	public SortedList getNewQueue(){
+		return this.newQueue;
+	}
+
+	public SortedList getArrivedQueue(){
+		return this.arrivedProcess;
+	}
+
+	public SortedList getCompletedQueue(){
+		return this.completedProcess;
+	}
+
+	synchronized public void insertIntoNewQueue(Process p){
+		newQueue.add(p);
+	}
+
+	synchronized public void insertIntoArrivedQueue(Process p){
+		arrivedProcess.add(p);
+	}
+
+	synchronized public void insertIntoCompletedQueue(Process p){
+		completedProcess.add(p);
+	}
+
+	synchronized public Process getProcessForScheduling(){
+		if(arrivedProcess.isEmpty()){
+			return null;
+		}else{
+			return (Process)arrivedProcess.removeFirst();
+		}
+	}
+
+	synchronized public void printAllProcess(){
+		Iterator i = newQueue.iterator();
+		System.out.println("New Queue");
+		while(i.hasNext()){
+			Process p =(Process)i.next();
+			System.out.println(p);
+		}
+		i = arrivedProcess.iterator();
+		System.out.println("Arrived Queue");  
+		while(i.hasNext()){
+			Process p =(Process)i.next();
+			System.out.println(p);
+		}
+		i = completedProcess.iterator();
+		System.out.println("Completed Queue");
+		while(i.hasNext()){
+			Process p =(Process)i.next();
+			System.out.println(p);
+		}
+	}
+
+	public void printAllProcessSummary(){
+		Iterator i = newQueue.iterator();
+		System.out.println("ALL PROCESS");
+		while(i.hasNext()){
+			Process p =(Process)i.next();
+			System.out.println(p);
+		}
+	}
+}
+
+class Time
+{
+	int current_time = 0;
+	int max_time = 20;
+	public Time(){
+
+	}
+
+	public int getTime(){
+		return this.current_time;
+	}
+
+	synchronized public void increamentTime(){
+		this.current_time = this.current_time + 1;
+	}
+
+	public boolean isOver(){
+		return ((max_time - current_time) > 0) ? true : false;
+	}
+}
+
+class InputThread extends Thread
+{
+	Input ip;
+	Time time;
+	int max_process_to_schedule = 10;
+
+	public InputThread(Input ip,Time time){
+		this.ip = ip;
+		this.time = time;
+	}
+
+	public void run(){
+		Random rand = new Random();
+		for(int i = 0;i<max_process_to_schedule;i++){
+			int pid = i;
+			int arrival_time = rand.nextInt(max_process_to_schedule) +  time.getTime();
+			int burst_time = rand.nextInt(3) + 1;
+			Process p = new Process(pid,arrival_time,burst_time);
+			ip.insertIntoNewQueue(p);
+			// ip.printAllProcess();
+		}
+	}
+}
+
+class Process
+{
+	int pid;
+	int arrival_time;
+	int burst_time;
+	int completion_time;
+
+	public Process(int pid,int arrival_time,int burst_time){
+		this.pid=pid;
+		this.arrival_time=arrival_time;
+		this.burst_time=burst_time;
+	}
+	public int getPid(){
+		return this.pid;
+	}
+	public int getArrivalTime(){
+		return this.arrival_time;
+	}
+	public int getBurstTime(){
+		return this.burst_time;
+	}
+	public void setCompletionTime(int completion_time){
+		this.completion_time = completion_time;
+	}
+  public String toString(){
+    return "PID "+this.pid +" AT " + this.arrival_time + " BT "+this.burst_time;
+  }
+}
+
+class SortByResponseRatio implements Comparator<Process>
+{
+
+	Time time;
+	public SortByResponseRatio(Time time){
+		this.time = time;
+	}
+
+
+	public int compare(Process p1, Process p2){
+
+		int current_time = time.getTime();
+		int p1_wait_time = p1.getArrivalTime() - current_time;
+		int p2_wait_time = p2.getArrivalTime() - current_time;
+		int p1_burst_time = p1.getBurstTime();
+		int p2_burst_time = p2.getBurstTime();
+
+		float p1_ratio = (float)(p1_wait_time + p1_burst_time) / p1_burst_time;
+		float p2_ratio = (float)(p2_wait_time + p2_burst_time) / p2_burst_time;
+
+		if(p1_ratio<p2_ratio){
+			return -1;
+		}else if(p2_ratio<p1_ratio){
+			return 1;
+		}else{
+			return 0;
+		}
+
+		// return p1.getArrivalTime() - p2.getArrivalTime();
+	}
+}
+
+class SortByArrivalTime implements Comparator<Process>
+{
+
+	public SortByArrivalTime(){
+	}
+
+
+	public int compare(Process p1, Process p2){
+		return p1.getArrivalTime() - p2.getArrivalTime();
+	}
+}
+
+class SortedList<Process> extends LinkedList<Process>
+{
+    private Comparator<Process> comparator;
+
+    public SortedList(final Comparator<Process> compare)
+    {
+        this.comparator = compare;
+    }
+
+    /**
+    * this ignores the index and delegates to .add() 
+    * so it will be sorted into the correct place immediately.
+    */
+    @Override
+    public void add(int index, Process element)
+    {
+        this.add(element);     
+    }
+
+    @Override
+    public boolean add(final Process e)
+    {
+        final boolean result = super.add(e);
+        Collections.sort(this, this.comparator);
+        return result;
+    }
+}
--- os_programs/synchronization algos/binarySemaphore.java
+++ os_programs/synchronization algos/binarySemaphore.java
@@ -2,13 +2,36 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
+
+class binarySemaphore
+{
+	public static void main(String[] args) throws Exception{
+		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int container_size = 10;
+		
+		Custom_Semaphore semaphore = new Custom_Semaphore();
+		Container container = new Container(container_size);
+		
+		Producer producer = new Producer(semaphore,container);
+		Consumer consumer = new Consumer(semaphore,container);
+		
+		producer.start();
+
+		consumer.start();
+
+		// while(producer.isAlive()||consumer.isAlive()){
+			// System.out.println("Producer Alive "+producer.isAlive()+" Consumer Alive "+consumer.isAlive());
+		// }
+	}
+}
+
 class Container{
 	int total_size;
 	int current_size;
 	ArrayList<Integer> currentItems;
 	
 	
-	Container(int size){
+	public Container(int size){
 		this.total_size = size;
 		this.current_size = 0;
 		currentItems = new ArrayList();
@@ -21,6 +44,7 @@ class Container{
 			System.out.println("produced "+ item);
 			return true;
 		}else{
+			System.out.println("Full");
 			return false;
 		}
 	}
@@ -32,13 +56,14 @@ class Container{
 			System.out.println("consumed " + consumed);
 			return true;
 		}else{
+			System.out.println("Empty");
 			return false;
 		}
 	}	
 }
 
-class Consumer extends Thread{
-	
+class Consumer extends Thread
+{
 	Custom_Semaphore semaphore;
 	Container container;
 	
@@ -49,19 +74,22 @@ class Consumer extends Thread{
 	
 	public void run(){
 		for(int i = 0;i < 20;i++){
-			semaphore.sem_wait();
-			//CS start
-			boolean flag = container.remove();
-			if(!flag){
-				i--;
-			}
-			//CS end
-			semaphore.sem_signal();
+			// while(true){
+				semaphore.sem_wait("Consumer");
+				//CS start
+				boolean flag = container.remove();
+				// if(!flag){
+				// 	i--;
+				// }
+				//CS end
+				semaphore.sem_signal();
+			// }
 		}
 	}
 }
 
-class Producer extends Thread{
+class Producer extends Thread
+{
 	
 	Custom_Semaphore semaphore;
 	Container container;
@@ -73,12 +101,12 @@ class Producer extends Thread{
 	
 	public void run(){
 		for(int i = 0;i < 20;i++){
-			semaphore.sem_wait();
+			semaphore.sem_wait("Producer");
 			//CS start
 			boolean flag = container.add(i);
-			if(!flag){
-				i--;
-			}
+			// if(!flag){
+			// 	i--;
+			// }
 			//CS end
 			semaphore.sem_signal();
 		}
@@ -87,39 +115,25 @@ class Producer extends Thread{
 
 class Custom_Semaphore{
 	//true means occupied, false means not occupied 
-	static boolean flag = false;
+	boolean flag = false;
+	String currentLock = "none";
 	
-	public void sem_wait(){
-		while(flag);
+	public void sem_wait(String name){
+		while(this.flag){
+			System.out.println("current " + name + " occupied "+this.currentLock);
+		}
 		this.flag = true;
+		this.currentLock = name;
+		System.out.println("Lock Acquired "+name);
 		
 	}
 	
 	public void sem_signal(){
 		this.flag = false;
+		System.out.println("Lock Released " + this.currentLock);
+		this.currentLock = "none";
 	}
 	
-}
-
-class binarySemaphore
-{
-	
-	public static void main(String[] args) throws Exception{
-		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int container_size = 10;
-		
-		Custom_Semaphore semaphore = new Custom_Semaphore();
-		Container container = new Container(container_size);
-		
-		Producer producer = new Producer(semaphore,container);
-		Consumer consumer = new Consumer(semaphore,container);
-		
-		producer.start();
-		consumer.start();
-		
-		
-		
-	}
 }
 
 
